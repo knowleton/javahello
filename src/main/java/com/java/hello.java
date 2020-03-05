81796c6baa0227165d0d0b13816cd1d9c80a60fa
knowleton <3106550557@qq.com>
2020-03-05 10:49:04 +0800

    test1

--- src/main/java/com/java/hello.java
+++ src/main/java/com/java/hello.java
@@ -2,5 +2,8 @@ package com.java;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+
+    }public static int add(int x,int y){
+        return x + y ;
     }
 }
